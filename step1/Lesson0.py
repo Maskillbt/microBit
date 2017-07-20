93bf72e50dab886ffb6ff5166005d699cd3d1186
ben-Maskill <ben.maskill@moneysupermarket.com>
2017-07-20 22:03:19 +0100

    Step 1

---
+++ step1/Lesson0.py
@@ -0,0 +1,4 @@
+from microbit import *
+
+#this is a comment and will not do anything but provids infomation on how the program is running
+display.show(Image.HAPPY)
---
+++ step1/Lesson1.py
@@ -0,0 +1,5 @@
+from microbit import *
+
+# this is a comment and will not do anything but givesinfomation on how
+# the program is running
+display.show(Image.HAPPY)
